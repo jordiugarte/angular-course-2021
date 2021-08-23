5aa6e6c09a2a6bdb1d0b26dc35424f97e1a4bb8a
jordiugarte <jordi933@gmail.com>
2021-08-23 18:44:19 -0400

    advance

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -15,22 +15,32 @@ import {
 })
 export class CardComponent implements OnInit {
   @Input('item') item: any;
+  @Output('update') update: EventEmitter<any> = new EventEmitter();
 
   @ViewChild('cardDiv') cardDiv: ElementRef;
 
   vaccined = false;
+  baseDisease = false;
+  adult = false;
+
+  vaccinable = false;
+
   color = '';
 
   constructor() {}
 
   ngOnInit() {
     this.vaccined = this.item.vaccined === 1 ? true : false;
+    this.baseDisease = this.item.disease;
+    this.adult = this.item.age >= 18;
     this.color = this.vaccined ? 'green' : 'red';
+    this.vaccinable = this.adult && !this.baseDisease && !this.vaccined;
   }
 
   vaccine() {
     this.item.vaccined = 1;
     this.ngOnInit();
     console.log('item', this.item);
+    this.update.emit(this.item);
   }
 }
