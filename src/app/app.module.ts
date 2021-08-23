c5e7d293aec51231b7b0c031a8fb8e44fdd8e1c1
jordiugarte <jordi933@gmail.com>
2021-08-23 19:38:46 -0400

    Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ProductModule } from './product/product.module';
+import { ClientModule } from './client/client.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ProductModule, ClientModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Client1Component } from './client1/client1.component';
 import { Client2Component } from './client2/client2.component';
+import { SharedModule } from '../shared/shared.module';
+import { ProviderModule } from '../provider/provider.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule, ProviderModule],
   declarations: [Client1Component, Client2Component],
   exports: [Client1Component, Client2Component]
 })
