31aba74e17142c0c22ea1b1f470b0962455649e4
jordiugarte <jordi933@gmail.com>
2021-09-03 18:40:02 -0400

    27/08/2021 Architecture of facebook

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  imports: [CommonModule],
+  declarations: [HomeComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: []
 })
 export class HomeModule {}
