44fc8b6253e9a8548b285f06ad23c5d68d275628
jordiugarte <jordi933@gmail.com>
2021-09-07 11:27:36 -0400

    01/09/2021 service with observable

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -8,10 +8,14 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Test1Service } from './services/test1.service';
 import { SingletonService } from './services/singleton.service';
+import { Login1Component } from './components/login1/login1.component';
+import { Login2Component } from './components/login2/login2.component';
+import { UtilsService } from './services/utils.service';
 
 const routes: Routes = [{ path: '', component: LoginComponent }];
+
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, Login1Component, Login2Component],
   imports: [
     MatProgressSpinnerModule,
     NgbToastModule,
@@ -21,6 +25,6 @@ const routes: Routes = [{ path: '', component: LoginComponent }];
     FormsModule,
     RouterModule.forChild(routes)
   ],
-  providers: [Test1Service]
+  providers: [Test1Service, UtilsService]
 })
 export class LoginModule {}
--- src/app/login/services/utils.service.ts
+++ src/app/login/services/utils.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable()
 export class UtilsService {
+
   private data$ = new BehaviorSubject<number>(0);
 
-  constructor() {}
+  constructor() {
 
-  public getData(): number {
+  }
+
+  public getData():number{
     return this.data$.getValue();
   }
 
-  public setData(num: number) {
+  public setData(num: number){
     this.data$.next(num);
   }
 
-  public currentData(): Observable<number> {
+  public currentData():Observable<number>{
     return this.data$.asObservable();
   }
+
+
 }
