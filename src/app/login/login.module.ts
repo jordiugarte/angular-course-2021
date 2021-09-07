69f31d5138d4b2add65b8fb6da8f5a01d8961075
jordiugarte <jordi933@gmail.com>
2021-09-07 11:25:29 -0400

    01/09/2021 singleton service and normal service

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -8,14 +8,10 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Test1Service } from './services/test1.service';
 import { SingletonService } from './services/singleton.service';
-import { Login1Component } from './components/login1/login1.component';
-import { Login2Component } from './components/login2/login2.component';
-import { UtilsService } from './services/utils.service';
 
 const routes: Routes = [{ path: '', component: LoginComponent }];
-
 @NgModule({
-  declarations: [LoginComponent, Login1Component, Login2Component],
+  declarations: [LoginComponent],
   imports: [
     MatProgressSpinnerModule,
     NgbToastModule,
@@ -25,6 +21,6 @@ const routes: Routes = [{ path: '', component: LoginComponent }];
     FormsModule,
     RouterModule.forChild(routes)
   ],
-  providers: [Test1Service, UtilsService]
+  providers: [Test1Service]
 })
 export class LoginModule {}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { RouterModule, Routes } from '@angular/router';
-import { SingletonService } from '../../login/services/singleton.service';
+import {RouterModule, Routes} from "@angular/router";
+import {SingletonService} from "../../login/services/singleton.service";
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  {path: '', component: HomeComponent}
+]
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
-  providers: [SingletonService]
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  providers: [
+    SingletonService
+  ]
 })
-export class HomeModule {}
+
+export class HomeModule { }
