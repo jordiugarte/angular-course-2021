7e99b64a6d882fa03c0fc008040fe201c045bf26
jordiugarte <jordi933@gmail.com>
2021-09-08 10:11:38 -0400

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing/routing.module';
-import { LugarComponent } from './lugar/lugar.component';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'jurados', pathMatch: 'full' },
-  {
-    path: 'jurados',
-    loadChildren: () =>
-      import('./jurados/jurados.module').then(m => m.JuradosModule)
-  },
-  {
-    path: 'votacion',
-    loadChildren: () =>
-      import('./votacion/votacion.module').then(m => m.VotacionModule)
-  },
-  { path: 'lugar', component: LugarComponent }
-];
 
 @NgModule({
   imports: [BrowserModule, FormsModule, RoutingModule],
--- src/app/votacion/votacion.module.ts
+++ src/app/votacion/votacion.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VotacionComponent } from './votacion.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [{ path: '', component: VotacionComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
   declarations: [VotacionComponent]
 })
 export class VotacionModule {}
