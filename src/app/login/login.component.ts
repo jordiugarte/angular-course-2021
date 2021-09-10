5856d42b16480928b17d601f22fd8affdcff0b5b
jordiugarte <jordi933@gmail.com>
2021-09-10 15:48:03 -0400

    06/09/2021 styles angula forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,17 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  name: string = '';
 
-  name:string = '';
+  constructor() {}
 
-  constructor() {
-
-  }
-
-  ngOnInit(): void {
-
-
-  }
-
-
+  ngOnInit(): void {}
 }
