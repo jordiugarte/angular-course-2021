8880714adfbf615fba9ed6b8ebfafcbb97596740
jordiugarte <jordi933@gmail.com>
2021-09-10 16:28:16 -0400

    09/09/2021 facebook login andn home design

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import {RouterModule, Routes} from "@angular/router";
-import {SingletonService} from "../../login/services/singleton.service";
+import { RouterModule, Routes } from '@angular/router';
+import { SingletonService } from '../../login/services/singleton.service';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { PostComponent } from './components/post/post.component';
 import { StoryComponent } from './components/story/story.component';
 import { MessageSenderComponent } from './components/message-sender/message-sender.component';
-import {MatCardModule} from "@angular/material/card";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {MatToolbarModule} from "@angular/material/toolbar";
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const routes: Routes = [
-  {path: '', component: HomeComponent}
-]
+const routes: Routes = [{ path: '', component: HomeComponent }];
 @NgModule({
   declarations: [
     HomeComponent,
@@ -33,8 +31,6 @@ const routes: Routes = [
     MatButtonModule,
     MatToolbarModule
   ],
-  providers: [
-    SingletonService
-  ]
+  providers: [SingletonService]
 })
-export class HomeModule { }
+export class HomeModule {}
