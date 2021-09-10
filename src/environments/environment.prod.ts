eb4846ba8018e72905b4a1b6e038fb565302b728
jordiugarte <jordi933@gmail.com>
2021-09-10 15:53:02 -0400

    08/09/2021 test 1000

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   app: {
-    apiBaseUrl: "https://facebook-angular-course-default-rtdb.firebaseio.com"
+    apiBaseUrl: 'https://facebook-angular-course-default-rtdb.firebaseio.com'
   }
 };
