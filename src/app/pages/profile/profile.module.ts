aec235ec3347b5b978fd4f3f92a9c4736696de0f
Jordi Ugarte <jordi933@gmail.com>
2021-09-15 15:10:15 -0400

    14/09/2021 design profile

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './profile.component';
+import {RouterModule, Routes} from "@angular/router";
+import {SharedMaterialModule} from "../../shared/shared-material.module";
+import {MessageSenderModule} from "../../shared/modules/message-sender/message-sender.module";
+import {PostModule} from "../../shared/modules/post/post.module";
 
+const routes: Routes = [
+  {path: '', component: ProfileComponent}
+]
 @NgModule({
-  imports: [
-    CommonModule
+  declarations: [
+    ProfileComponent
   ],
-  declarations: []
+  imports: [
+    CommonModule,
+    MessageSenderModule,
+    PostModule,
+    RouterModule.forChild(routes),
+    SharedMaterialModule
+  ]
 })
 export class ProfileModule { }
