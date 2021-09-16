8d2521926f3973030e1665e6b0ee9e7a5c788633
jordiugarte <jordi933@gmail.com>
2021-09-16 19:58:13 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,34 @@ import { WalletComponent } from './wallet/wallet.component';
 import { DateFormatPipe } from './date-format.pipe';
 import { HttpClientModule } from '@angular/common/http';
 import { Api } from './home/services/api.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
+  exports: [
+    MatProgressSpinnerModule,
+    MatSliderModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
   declarations: [
     AppComponent,
     DateFormatPipe,
