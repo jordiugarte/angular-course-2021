4ef2da377229d61a63501e228fe6fe34cd5efa1d
jordiugarte <jordi933@gmail.com>
2021-09-16 17:23:34 -0400

    2 files modified

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Api } from '../home/services/api.service';
 
 @Component({
   selector: 'app-wallet',
   templateUrl: './wallet.component.html',
-  styleUrls: ['./wallet.component.css']
+  styleUrls: ['./wallet.component.css'],
 })
 export class WalletComponent implements OnInit {
+  @Input('item') item: any;
+  @Output('update') update: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
