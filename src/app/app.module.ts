1630afb5e9d6280c400b54770e260bcd1d40a774
jordiugarte <jordi933@gmail.com>
2021-09-16 17:09:09 -0400

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TransactionComponent } from './transaction/transaction.component';
+import { HomeComponent } from './home/home.component';
 import { WalletComponent } from './wallet/wallet.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, TransactionComponent, WalletComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TransactionComponent,
+    WalletComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Api } from '../../home/services/api.service';
 
 @Component({
-  selector: 'app-transaction',
-  templateUrl: './transaction.component.html',
-  styleUrls: ['./transaction.component.css']
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
 })
 export class TransactionComponent implements OnInit {
+  @Input('item') item: any;
+  @Output('update') update: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  @ViewChild('cardDiv') cardDiv: ElementRef;
+
+  vaccined = false;
+  baseDisease = false;
+  adult = false;
+
+  vaccinable = false;
+
+  color = '';
+
+  constructor(private service: Api) {}
 
   ngOnInit() {
+    this.vaccined = this.item.vaccined === 1 ? true : false;
+    this.baseDisease = this.item.disease;
+    this.adult = this.item.age >= 18;
+    this.color = this.vaccined ? 'green' : 'red';
+    this.vaccinable = this.adult && !this.baseDisease && !this.vaccined;
   }
 
+  vaccine() {
+    var maxDoses = 0;
+    var currentDoses = this.item.doses;
+    switch (this.item.vaccineType) {
+      case 'A':
+        maxDoses = 1;
+      case 'B':
+        maxDoses = 2;
+      case 'C':
+        maxDoses = 3;
+    }
+    if (currentDoses >= maxDoses - 1) {
+      this.item.vaccined = 1;
+      this.service.delete(this.item.key).subscribe((s) => {
+        this.service.vaccine(this.item).subscribe((s) => {
+          this.update.emit(this.item);
+        });
+      });
+    } else {
+      this.item.doses++;
+      this.service.addDose(this.item.key, this.item).subscribe((s) => {
+        this.update.emit(this.item);
+      });
+    }
+  }
 }
